52f16c137d099870e10c1f6cfef0a82904aaafa4
Satvik Sharma <87787530+sharmasatvik989@users.noreply.github.com>
2022-01-20 23:28:49 -0600

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,11 +10,11 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
+          {emoji("Developed by Satvik Sharma")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
+          Theme by{"By Satvik Sharma with special contribtution from DeveloperFolio"}
+          <a href="https://github.com/sharmasatvik989">
             developerFolio
           </a>
         </p>
